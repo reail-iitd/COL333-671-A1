7d66f84649e85523105365abe044a5315362e4c4
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2020-10-22 13:32:47 +0530

    Removed YOUR CODE HERE from places not required

--- searchAgents.py
+++ searchAgents.py
@@ -508,7 +508,6 @@ class ClosestDotSearchAgent(SearchAgent):
         walls = gameState.getWalls()
         problem = AnyFoodSearchProblem(gameState)
 
-        "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -550,7 +549,6 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         """
         x,y = state
 
-        "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
 
 def mazeDistance(point1, point2, gameState):
